f871604e7c77af5e84e737dcd8d7dd58ea2de9cd
guts111 <46725535@qq.com>
2018-10-17 11:15:15 +0800

    eject

---
+++ src/components/SiteHeader.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class SiteHeader extends Component {
+  render () {
+    return (
+      <div className="fixed left-0 top-0 right-0 h3  bg-light-yellow tc v-mid">
+        <div className="flex items-center">
+          <div className="">
+            三
+          </div>
+          <div className="flex-auto self-end">sdf</div>
+
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default SiteHeader
